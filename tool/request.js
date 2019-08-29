9deb0354b6f149398564ee69f803c79ec3069713
huoqingchi <123203557@qq.com>
2019-08-29 17:00:18 +0800

    修改request为开发环境

--- tool/request.js
+++ tool/request.js
@@ -1,5 +1,5 @@
-// const url_prefix = '/api';// 开发环境
-const url_prefix = '/aaa';// 开发环境
+const url_prefix = '/api';// 开发环境
+// const url_prefix = '/aaa';// 开发环境
 // const url_prefix = '';//本地测试环境
 // var url_prefix = "https://meizi.manogue.com.cn";//线上环境
 
