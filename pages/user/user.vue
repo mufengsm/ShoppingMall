413fc9e10f7cbca577514453aeccb5ad7f8cf7f5
huoqingchi <123203557@qq.com>
2019-09-03 16:08:24 +0800

    完成我的tabber，还需要完成行为

--- pages/user/user.vue
+++ pages/user/user.vue
@@ -1,6 +1,5 @@
 <template>
     <view class="container">
-
 		<view class="user-section">
 			<view class="header_bg"></view>
 			<view class="user-info-box">
@@ -18,8 +17,7 @@
 			<view class="order-section">
 				<view class="my_order">
 					<text class="left">我的订单</text>
-					
-					<text class="right">全部订单<i class="iconfont icon-youjiantou"></i></text>
+					<text class="right">全部订单<span class="iconfont icon-youjiantou"></span></text>
 				</view>
 					<view class="order-item" v-for="(item,index) in order_status" :key="index">
 						<image :src="item.img_url" mode="aspectFit"></image>
@@ -28,18 +26,32 @@
 			</view>
 		</view>
 		<view class="tj-sction">
-			<view class="tj-item">
-				<text class="num">128.8</text>
-				<text>余额</text>
+			<image class="recommend_img" src="../../static/images/my_icon2_red.png" mode="aspectFit"></image>
+			<text class="recommend_txt">推广有礼</text>
+		</view>
+		<view class="my_wallet">
+			<view class="order-section">
+				<view class="my_order">
+					<text class="left">我的钱包 <text class="is_opening">未开通,去开通</text></text>
+					<text class="right">资金管理<span class="iconfont icon-youjiantou"></span></text>
+				</view>
 			</view>
-			<view class="tj-item">
-				<text class="num">0</text>
-				<text>优惠券</text>
+			<view class="my_wallet_items">
+				<view class="wallet_item" v-for="(item,index) in my_wallet_item" :key="index">
+					<view class="num">{{item.num}}</view>
+					<view class="text">{{item.text}}</view>
+				</view>
 			</view>
-			<view class="tj-item">
-				<text class="num">20</text>
-				<text>积分</text>
-			</view>
+		</view>
+		<view class="other_function">
+			<image 
+			:src="item.img_url" 
+			mode="aspectFit"  
+			class="other_function_item" 
+			v-for="(item,index) in other_functions" 
+			:key="index" 
+			>
+			</image>
 		</view>
     </view>
 </template>
@@ -61,10 +73,24 @@ export default {
       coverTransition: '0s',
       moving: false,
 	  order_status:[
-		  {img_url:"../../static/images/myct_indentl_grey__01.png",text:"全部订单"},
-		  {img_url:"../../static/images/myct_indentl_grey__03.png",text:"待付款"},
+		  {img_url:"../../static/images/myct_indentl_grey__01.png",text:"待付款"},
+		  {img_url:"../../static/images/myct_indentl_grey__03.png",text:"待发货"},
 		  {img_url:"../../static/images/myct_indentl_grey__05.png",text:"待收货"},
-		  {img_url:"../../static/images/myct_indentl_grey__07.png",text:"退款/售后"}
+		  {img_url:"../../static/images/myct_indentl_grey__07.png",text:"带评价"},
+		  {img_url:"../../static/images/myct_indentl_grey__09.png",text:"退换货"}
+	  ],
+	  my_wallet_item:[
+		  {num:"--",text:"余额"},
+		  {num:0,text:"积分"},
+		  {num:0,text:"优惠卷"},
+		  {num:0,text:"银行卡管理"},
+	  ],
+	  other_functions:[
+		  {img_url:"../../static/images/myct_others_i_red_01.png"},
+		  {img_url:"../../static/images/myct_others_i_red_13.png"},
+		  {img_url:"../../static/images/myct_others_i_red_19.png"},
+		  {img_url:"../../static/images/myct_others_i_red_06.png"},
+		  {img_url:"../../static/images/myct_others_i_red_21.png?v=1"}
 	  ]
     };
   },
@@ -168,18 +194,23 @@ export default {
 		padding-bottom: 20upx;
 	}
 	.tj-sction{
-		@extend %section;
-		.tj-item{
-			@extend %flex-center;
-			flex-direction: column;
-			height: 140upx;
-			font-size: $font-sm;
-			color: #75787d;
+		width: 100%;
+		padding: 24upx 50upx;
+		border-bottom:25upx solid #f5f5f5;
+		overflow: hidden;
+		.recommend_img{
+			width: 12vw;
+			height: 12vw;
+			float: left;
 		}
-		.num{
-			font-size: $font-lg;
-			color: $font-color-dark;
-			margin-bottom: 8upx;
+		.recommend_txt{
+			margin-left:20upx;
+			float: left;
+			font-weight: 400;
+			font-size: 17px;
+			color: #4c4c4c;
+			display: block;
+			line-height:96upx;
 		}
 	}
 	.order-section{
@@ -190,9 +221,19 @@ export default {
 		flex-wrap:wrap;
 		.my_order{
 			width: 100%;
-			border: 1px solid red;
+			padding: 20upx 0;
 			.right{
 				float: right;
+				color: #999;
+				font-size:16px;
+				.icon-youjiantou{
+					font-weight: 700;
+					font-size: 20px;
+				}
+			}
+			.left{
+				padding-left: 30upx;
+				font-size: 17px;
 			}
 		}
 		.order-item{
@@ -202,6 +243,9 @@ export default {
 			border-radius: 10upx;
 			font-size: $font-sm;
 			color: $font-color-dark;
+			image{
+				width: 55%;	
+			}
 		}
 		.yticon{
 			font-size: 48upx;
@@ -212,5 +256,49 @@ export default {
 			font-size:44upx;
 		}
 	}
-	
+	.my_wallet{
+		.order-section{
+			height: auto;
+			padding: 0;
+			.my_order{
+				.left{
+					padding-left: 30px;
+					.is_opening{
+						font-size: 14px;
+						color: #df0e1d;
+						padding-left: 6px;
+						position: relative;
+						top: 1px;
+					}
+				} 
+			}
+		}
+		.my_wallet_items{
+			width: 100%;
+			margin-top:22upx;
+			border-bottom:25upx solid #f5f5f5;
+			overflow: hidden;
+			flex-wrap:wrap;
+			.wallet_item{
+				float: left;
+				margin-bottom: 22upx;
+				width: 33.333%;
+				text-align: center;
+				.num{
+					color: #e23c3c;
+				}
+				.text{
+					font-size: 15px;
+				}
+			}
+		}
+	}
+	.other_function{
+		height: 400upx;
+		.other_function_item{
+			width: 25%;
+			height: 200upx;
+			float: left;
+		}
+	}
 </style>
