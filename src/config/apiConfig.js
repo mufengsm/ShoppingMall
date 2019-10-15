0d3f9e58acc6cbb297952f7b180f7b7e5cb3bbd4
Rope <scarf666@163.com>
2019-10-15 14:03:01 +0800

    声明常量存储apiUrl

--- src/config/apiConfig.js
+++ src/config/apiConfig.js
@@ -12,10 +12,10 @@ export const GET_MERCHANDISE_RECOMMENDATION = locaApi+'/v1/goods/recommend';
 // 用户登录
 export const POST_USER_LOGIN = locaApi+'/v1/user/login';
 // 搜索页面产品信息
-export const POST_BRAND_LIBRARY_QUERY = networkApi+'/v4/goods/goods_list';
+export const POST_BRAND_LIBRARY_QUERY = locaApi+'/v4/goods/goods_list';
 // 控区控价信息列表
-export const POST_BRAND_LIST = formalNetworkApi+'/v4/Brands/index';
+export const POST_BRAND_LIST = locaApi+'/v4/Brands/index';
 // 查询品牌
-export const POST_QUERY_BRAND = networkApi+'/v4/goods/brand_list';
+export const POST_QUERY_BRAND = locaApi+'/v4/goods/brand_list';
 // 查询分类
-export const POST_QUERY_CLASSIFICATION = networkApi+'/v4/goods/cate_list';
+export const POST_QUERY_CLASSIFICATION = locaApi+'/v4/goods/cate_list';
