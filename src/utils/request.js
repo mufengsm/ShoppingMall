9eace4f3149d14abb2d2cd719e537e6fe1e18212
Rope <scarf666@163.com>
2019-10-31 18:01:29 +0800

    添加get请求queryString变为data

--- src/utils/request.js
+++ src/utils/request.js
@@ -3,11 +3,22 @@ export default {
     access_token: uni.getStorageSync('access_token') ? uni.getStorageSync('access_token') : " "
   },
   async GET(dataObj) {
+    // 这个函数是为了过滤get请求的data
+    const parseQuery = (obj) => {
+      let str = ''
+      for (let key in obj) {
+        const value = typeof obj[key] === 'string'
+          ? obj[key]
+          : JSON.stringify(obj[key])
+        str += '&' + key + '=' + value
+      }
+      return str.substr(1)
+    }
     const [error, res] = await uni.request({
       header:{
         'Token': this.storage.access_token,
       },
-      url: dataObj.url,
+      url: dataObj.url += '?' + parseQuery(dataObj.data),
       method: 'GET',
     });
     return res.data;
