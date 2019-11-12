6342e63c845580caea5512286b7c96c4e31cf2ae
Rope <scarf666@163.com>
2019-11-12 11:19:58 +0800

    自己写未完成城市选择

--- src/pages/mys/addressForm/addressForm.vue
+++ src/pages/mys/addressForm/addressForm.vue
@@ -3,24 +3,27 @@
         <picker-view
             v-if="visible"
             :indicator-style="indicatorStyle"
-            :value="value"
             @change="bindChange"
 			class="picker"
         >
             <picker-view-column>
                 <view 
 				class="item" v-for="(item,index) in largeArea" :key="index" 
-				:data-id="item.id">{{item.name}}</view>
-            </picker-view-column>
-            <!-- <picker-view-column>
-                <view class="item" v-for="(item,index) in months" :key="index">{{item}}月</view>
+				>{{item.name}}</view>
             </picker-view-column>
             <picker-view-column>
-                <view class="item" v-for="(item,index) in days" :key="index">{{item}}日</view>
+                <view 
+                class="item" v-for="(item,index) in cityClass" :key="index"
+                >{{item.name}}</view>
             </picker-view-column>
-			<picker-view-column>
-                <view class="item" v-for="(item,index) in days" :key="index">{{item}}日</view>
-            </picker-view-column> -->
+            <picker-view-column>
+                <view class="item" v-for="(item,index) in area" :key="index"
+                >{{item.name}}</view>
+            </picker-view-column>
+            <picker-view-column v-if="street.length">
+                <view class="item" v-for="(item,index) in street" :key="index"
+                >{{item.name}}</view>
+            </picker-view-column>
         </picker-view>
     </view>
 </template>
@@ -28,53 +31,46 @@
 <script>
 export default {
     data: function() {
-        const date = new Date();
-        const years = [];
-        const year = date.getFullYear();
-        const months = [];
-        const month = date.getMonth() + 1;
-        const days = [];
-        const day = date.getDate();
-        for (let i = 1990; i <= date.getFullYear(); i++) {
-            years.push(i);
-        }
-        for (let i = 1; i <= 12; i++) {
-            months.push(i);
-        }
-        for (let i = 1; i <= 31; i++) {
-            days.push(i);
-        }
         return {
             title: "picker-view",
-
-            value: [9999, month - 1, day - 1],
             visible: true,
             indicatorStyle: `height: ${Math.round(
                 uni.getSystemInfoSync().screenWidth / (750 / 100)
 			)}px;`,
-			largeArea:[]
+            largeArea:[],
+            cityClass:[],
+            area:[],
+            street:[],
         };
 	},
 	onLoad(){
-		this.watchCity(1).then(res=>{
-			this.largeArea = res;
-		})
+       this.lodingCity(1,37,567)
 	},
     methods: {
         bindChange: function(e) {
-			const val = e.detail.value;
-			console.log(this.largeArea[val]);
-			
-            // this.year = this.years[val[0]];
-            // this.month = this.months[val[1]];
-            // this.day = this.days[val[2]];
+            const val = e.detail.value;
+            const province = this.largeArea[val[0]];
+            const city = this.cityClass[val[1]];
+            const area = this.area[val[2]];
+            const street = this.street[val[3]];
+            if(this.street){
+                this.lodingCity()
+			console.log(this.largeArea[val[0]]);
+            console.log(this.cityClass[val[1]]);
+            console.log(this.area[val[2]]);
+            console.log(this.street[val[3]]);
+            }else{
+            console.log(this.largeArea[val[0]]);
+            console.log(this.cityClass[val[1]]);
+            console.log(this.area[val[2]]);
+            }
 		},
-		watchCity(level="",id=""){
-			
+		watchCity(id="",level=""){
 			// 过去城市数据，只需要传id
 			return this.$request.POST({
 				url:this.$api.apiUrl.GET_CITY,
 				data:{
+                    id:id,
 					level:level
 				}
 			}).then(res=>{
@@ -82,7 +78,25 @@ export default {
 					return res.data
 				}
 			})
-		}
+        },
+        lodingCity(city,area,street){
+             // 获取默认省
+            this.watchCity("",1).then(res=>{
+                this.largeArea = res;
+            })
+            // 获取默认市
+            this.watchCity(city).then(res=>{
+                this.cityClass = res;
+            })
+            // 获取默认区
+            this.watchCity(area).then(res=>{
+                this.area = res;
+            })
+            // 获取街道
+            this.watchCity(street).then(res=>{
+                this.street = res;
+            })
+        }
     }
 };
 </script>
