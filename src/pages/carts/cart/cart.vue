4dfefeebf3c904e8ccd2453aff7104dd1ce67e29
Rope <scarf666@163.com>
2019-11-13 17:57:26 +0800

    修改page背景

--- src/pages/carts/cart/cart.vue
+++ src/pages/carts/cart/cart.vue
@@ -1,5 +1,5 @@
 <template>
-	<view v-if="isLoding" class="container" :style="{height:'100vh'}">
+	<view v-if="isLoding" class="container">
 		<!-- 空白页 -->
 		<view class="empty" v-if="!cartData.length">
 			<image src="https://meizi.manogue.com.cn/static/wap/images/emptycart.png" mode="scaleToFill"></image>
